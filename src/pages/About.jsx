62a4b162b3e98779ff163ae69635e307c0109c72
Mostafa Raihan <raihan.invite@gmail.com>
2026-03-10 13:01:34 +0600

    add;

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const About = () => {
     return <div>
         <h1>This About Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel itaque recusandae eligendi deleniti facilis quam suscipit optio ipsa ex ipsum. Nostrum rem adipisci quibusdam sunt consectetur rerum ex delectus ea temporibus inventore sequi non vel, quam doloribus odit accusantium similique laborum. Debitis, rerum unde suscipit vel corporis impedit quaerat eligendi explicabo maxime, illum veritatis itaque voluptas quisquam, atque ut esse mollitia odio et tenetur. Ullam deserunt error voluptatem perferendis at laudantium velit eius similique tempora debitis enim accusantium vitae, illum dolore, illo architecto inventore vel harum corrupti omnis sunt consequatur accusamus cumque officia. Enim temporibus quod asperiores sint similique ab, at eveniet, est exercitationem nostrum veritatis cum excepturi sunt? Facere, tempore.</p>
     </div>
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Home = () => {
     return <div>
         <h1>This Home Page</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda fugit vitae libero saepe, a accusamus quisquam totam quibusdam recusandae, magnam facere ducimus quidem ipsam quod fuga soluta. Et hic vero tempore debitis molestias porro nostrum quam cum, recusandae accusamus magnam corporis libero vel optio inventore, natus, minus aliquam. Delectus cum distinctio id vero veniam vitae dolore, quibusdam accusamus saepe corporis fuga itaque minus labore nemo incidunt obcaecati mollitia atque tempore, eveniet dolorem laboriosam porro possimus. Iusto exercitationem in amet, suscipit blanditiis quae soluta ab officiis, quaerat odit tempora commodi reprehenderit magnam itaque esse molestias? Hic rem harum eum dicta exercitationem sapiente non delectus beatae ratione voluptatum laudantium tempora animi, doloribus dolorem!</p>
     </div>
 }
 
